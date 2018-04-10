85cf55fd4c97a0265fa5ec715343fbb8e5422a00
Belguith Omar <omar.belguith@medtech.tn>
2018-04-10 16:49:35 +0100

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { TodayComponent } from './components/today/today.component';
+import { WeeklyComponent } from './components/weekly/weekly.component';
+import { MonthlyComponent } from './components/monthly/monthly.component';
+import { YearlyComponent } from './components/yearly/yearly.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent },
+  {path: 'login', component: LoginComponent },
+  {path: 'signup', component: SignupComponent },
+  {path: 'daily', component: TodayComponent },
+  {path: 'weekly', component: WeeklyComponent},
+  {path: 'monthly', component:MonthlyComponent },
+  {path: 'yearly', component:YearlyComponent },
+  {path: '',redirectTo: '/home',pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    TodayComponent,
+    WeeklyComponent,
+    MonthlyComponent,
+    YearlyComponent,
+    PageNotFoundComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
