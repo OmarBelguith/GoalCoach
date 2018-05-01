982de852afee328c6c10e2e5b78d66daab0f3830
Belguith Omar <omar.belguith@medtech.tn>
2018-05-01 17:13:47 +0100

    fixing prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC2YUpPWgQbuzu3Zmun2PrZrZnF4RhtlnA",
+    authDomain: "goalcoach-44b58.firebaseapp.com",
+    databaseURL: "https://goalcoach-44b58.firebaseio.com",
+    projectId: "goalcoach-44b58",
+    storageBucket: "goalcoach-44b58.appspot.com",
+    messagingSenderId: "475790547289"
+  }
 };
+
